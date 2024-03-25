23b93a553a9e6c0bf3a4dbf39109ddcc7ba12f9a
gwen windflower <gwenwindflower@gmail.com>
2024-03-25 16:51:11 -0500

    Compile new requirements.txt via uv

--- setup.py
+++ setup.py
@@ -1,59 +1,20 @@
 from setuptools import find_packages, setup
 
 
-def read_requirements(filename: str):
-    with open(filename) as requirements_file:
-        import re
-
-        def fix_url_dependencies(req: str) -> str:
-            """Pip and setuptools disagree about how URL dependencies should be handled."""
-            m = re.match(
-                r"^(git\+)?(https|ssh)://(git@)?github\.com/([\w-]+)/(?P<name>[\w-]+)\.git", req
-            )
-            if m is None:
-                return req
-            else:
-                return f"{m.group('name')} @ {req}"
-
-        requirements = []
-        for line in requirements_file:
-            line = line.strip()
-            if line.startswith("#") or len(line) <= 0:
-                continue
-            requirements.append(fix_url_dependencies(line))
-    return requirements
+def parse_requirements(filename):
+    """Load requirements from a pip requirements file."""
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
 
 
-# version.py defines the VERSION and VERSION_SHORT variables.
-# We use exec here so we don't import cached_path whilst setting up.
-VERSION = {}  # type: ignore
-with open("jafgen/version.py", "r") as version_file:
-    exec(version_file.read(), VERSION)
-
+requirements = parse_requirements("requirements.in")
 setup(
-    name="jafgen",
-    version=VERSION["VERSION"],
-    description="",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    classifiers=[
-        "Intended Audience :: Science/Research",
-        "Development Status :: 3 - Alpha",
-        "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 3",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence",
-    ],
-    keywords="",
-    url="https://github.com/dbt-labs/jaffle-shop-generator",
-    author="dbt Labs™️",
-    author_email="winnie@dbtlabs.com",
-    license="Apache",
+    name="jaffle-shop-generator",
+    version="0.2",
     packages=find_packages(
         exclude=["*.tests", "*.tests.*", "tests.*", "tests"],
     ),
-    package_data={"jafgen": ["py.typed"]},
-    install_requires=read_requirements("requirements.txt"),
-    extras_require={"dev": read_requirements("dev-requirements.txt")},
-    python_requires=">=3.10",
-    entry_points={"console_scripts": ["jafgen=jafgen.main:main"]},
+    package_dir={"jafgen": "jafgen"},
+    entry_points={"console_scripts": ["jafgen = jafgen.main:main"]},
+    install_requires=requirements,
 )
