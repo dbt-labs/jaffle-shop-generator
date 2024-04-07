4737d4853333a6097f671452480445dceb033695
gwen windflower <gwenwindflower@gmail.com>
2024-04-07 15:21:44 -0500

    Add pyproject.toml with discrete deps listed

--- setup.py
+++ setup.py
@@ -1,20 +1,12 @@
 from setuptools import find_packages, setup
 
-
-def parse_requirements(filename):
-    """Load requirements from a pip requirements file."""
-    lineiter = (line.strip() for line in open(filename))
-    return [line for line in lineiter if line and not line.startswith("#")]
-
-
-requirements = parse_requirements("requirements.in")
 setup(
     name="jaffle-shop-generator",
-    version="0.4.0",
+    version="0.4.1",
     packages=find_packages(
         exclude=["*.tests", "*.tests.*", "tests.*", "tests"],
     ),
     package_dir={"jafgen": "jafgen"},
     entry_points={"console_scripts": ["jafgen = jafgen.cli:app"]},
-    install_requires=requirements,
+    install_requires=["numpy", "pandas", "Faker", "typer[all]"],
 )
