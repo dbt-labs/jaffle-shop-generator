723428b54d1aeeda4ca883e3408f7741af682e73
gwen windflower <gwenwindflower@gmail.com>
2024-04-07 15:57:16 -0500

    Update setup.py and pyproject.toml with description and README info

--- setup.py
+++ setup.py
@@ -6,6 +6,10 @@ setup(
     packages=find_packages(
         exclude=["*.tests", "*.tests.*", "tests.*", "tests"],
     ),
+    authors=["gwenwindflower", "drewbanin"],
+    description="A synthetic data generator CLI for a fictional Jaffle Shop",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     package_dir={"jafgen": "jafgen"},
     entry_points={"console_scripts": ["jafgen = jafgen.cli:app"]},
     install_requires=["numpy", "pandas", "Faker", "typer[all]"],
