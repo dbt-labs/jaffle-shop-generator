5b6472bceecf989a242779e5e1ad7a782ba107ad
gwen windflower <gwenwindflower@gmail.com>
2024-04-10 12:19:02 -0500

    Bump version to 0.4.12

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="jafgen",
-    version="0.4.11",
+    version="0.4.12",
     packages=find_packages(
         exclude=["*.tests", "*.tests.*", "tests.*", "tests"],
     ),
