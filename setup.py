c1c742d13ae3484fc38721e59192ba84d646e6ef
gwen windflower <gwenwindflower@gmail.com>
2024-04-27 17:41:31 -0500

    chore(bump version)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="jafgen",
-    version="0.4.13",
+    version="0.4.14",
     packages=find_packages(
         exclude=["*.tests", "*.tests.*", "tests.*", "tests"],
     ),
