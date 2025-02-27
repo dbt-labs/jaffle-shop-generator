47b1e35e564157ccee4b676d73c3618985512970
Joost Boonzajer Flaes <joostboon@gmail.com>
2025-02-27 16:07:30 +0200

    Added option to have days

--- jafgen/cli.py
+++ jafgen/cli.py
@@ -9,14 +9,24 @@ app = typer.Typer()
 
 @app.command()
 def run(
+    # We set default to 0 here to make sure users don't get confused if they only put in days.
+    # If they don't set anything we have years default = 1 later to keep the same functionality. 
     years: Annotated[
-        int, typer.Argument(help="Number of years to simulate. Default is 1.")
-    ] = 1,
+         int, typer.Argument(help="Number of years to simulate. Default is 1.")
+    ] = 0,
+    days: Annotated[
+        int, typer.Option(help="Number of years to simulate. Default is 1.")
+    ] = 0,
     pre: Annotated[
         str,
         typer.Option(help="Optional prefix for the output file names."),
     ] = "raw",
 ) -> None:
-    sim = Simulation(years, pre)
+    
+    # To keep the default value for backwards compatibility.
+    if years == 0 and days == 0:
+        years = 1
+
+    sim = Simulation(years, days, pre)
     sim.run_simulation()
     sim.save_results()
